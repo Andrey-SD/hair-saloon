7a5356172955e0737fe59b037607156eefdbcc6c
Andrey-SD <andrey.sd.post@gmail.com>
2018-02-12 22:17:11 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Образци шаблонов</title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<h1>Шаблоны сайтов</h1>
+	<div>
+		<a href="template1/index.html"><img src="images/temp1.bmp" alt="temp1"><p>Шаблон №1</p></a>
+		<a href="template2/index.html"><img src="images/temp2.bmp" alt="temp2"><p>Шаблон №2</p></a>
+		<a href="template3/index.html"><img src="images/temp3.bmp" alt="temp2"><p>Шаблон №3</p></a>
+	</div>
+</body>
+</html>
